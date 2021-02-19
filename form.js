8891752e3c61e2e3df6fc29256ba2e980c626128
savanahatieno <savanaht01@gmail.com>
2021-02-19 20:53:38 +0300

    Initial changes

--- form.js
+++ form.js
@@ -13,8 +13,14 @@ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
 var name =(document.getElementById("name").value);
 var date = parseInt(document.getElementById("date").value);
+
 var date0fbirth = new Date(year + "/" + month + "/" + day);
   var results = date0fbirth.getDay();
   var output = document.getElementById("output");
   var male = document.getElementById("male")
   var female = document.getElementById("female")
+
+  if (month =="" && year =="" && day =="" && name=="") {
+    alert("Please Enter you credentials");
+    return false;
+  }
