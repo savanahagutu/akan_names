518a97771dec6349c019cbb69bbb4f9d43f06f4b
savanahatieno <savanaht01@gmail.com>
2021-02-19 20:39:17 +0300

    Linked javascript to form

---
+++ form.js
@@ -0,0 +1,20 @@
+var day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+var name =(document.getElementById("name").value);
+var date = parseInt(document.getElementById("date").value);
+var date0fbirth = new Date(year + "/" + month + "/" + day);
+  var results = date0fbirth.getDay();
+  var output = document.getElementById("output");
+  var male = document.getElementById("male")
+  var female = document.getElementById("female")
